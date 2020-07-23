ac17a2879abb949ff6d3376e662a6e9a1daad0ea
radifar <m.radifar05@gmail.com>
2020-07-23 15:35:12 +0700

    add url to setup.py

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     setup_requires=[] + pytest_runner,
 
     # Additional entries you may want simply uncomment the lines you want and fill in the data
-    # url='http://www.my_package.com',  # Website
+    url='https://github.com/radifar/PyPLIF-HIPPOS',
     # install_requires=[],              # Required packages, pulls from pip if needed; do not use for Conda deployment
     # platforms=['Linux',
     #            'Mac OS-X',
