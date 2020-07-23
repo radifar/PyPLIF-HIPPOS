516a0fbe37b588a74ea3969d252e5bae37ada859
radifar <m.radifar05@gmail.com>
2020-07-23 20:27:43 +0700

    first test done

--- hippos/tests/test_hippos.py
+++ hippos/tests/test_hippos.py
@@ -3,10 +3,10 @@ Unit and regression test for the pyplif_hippos package.
 """
 
 # Import package, test suite, and other packages as needed
-import pyplif_hippos
+import hippos
 import pytest
 import sys
 
 def test_pyplif_hippos_imported():
     """Sample test, will always pass so long as import statement worked"""
-    assert "pyplif_hippos" in sys.modules
+    assert "hippos" in sys.modules
