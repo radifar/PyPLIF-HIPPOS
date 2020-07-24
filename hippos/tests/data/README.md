6474ecaa497b309fd2023724561b268ae6d73d2a
radifar <m.radifar05@gmail.com>
2020-07-24 17:05:28 +0700

    add basic parse_vina_conv test

---
+++ hippos/tests/data/README.md
@@ -0,0 +1,5 @@
+# Sample Package Data
+
+This directory contain the data required for testing. As integrating the content
+into test function became impractical (e.g. inserting protein structure in Python
+script). Also separating the test data with the script make the test code cleaner.
--- hippos/tests/test_parse_docking_conf.py
+++ hippos/tests/test_parse_docking_conf.py
@@ -6,8 +6,17 @@ Test for functions in hippos.initialize.parse_docking_conf
 from hippos import parse_vina_conf, parse_plants_conf
 import pytest
 import os, sys
+from openbabel import OBMol
 
-def test_parse_vina_conf(tmpdir):
+def test_parse_vina_conf():
     """Test parsing AutoDock Vina configuration"""
+    print(os.getcwd())
+    docking_results = parse_vina_conf('hippos/tests/data/vina/vina-001.conf')
 
-    assert 0 == 0
+    assert isinstance(docking_results['protein'], OBMol)
+    assert isinstance(docking_results['docked_ligands'][0], OBMol)
+    assert len(docking_results['docked_ligands']) == 20
+
+    assert docking_results['docked_proteins'] == []
+    assert docking_results['scorelist'][0] == '-6.8'
+    assert docking_results['mollist'][0] == '247120_1'
