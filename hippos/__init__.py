4870b1942d3ac04c40996383d926b18c2537ec17
radifar <m.radifar05@gmail.com>
2020-07-24 16:31:34 +0700

    prepare parse_docking_conv test

--- hippos/__init__.py
+++ hippos/__init__.py
@@ -5,12 +5,16 @@ HIPPOS Is PyPLIF On Steroids. A Molecular Interaction Fingerprinting Tool for Do
 
 from __future__ import print_function
 
+from openbabel import OBMol, OBConversion
+
 # Solution to use simple import for dual way installation
 # https://stackoverflow.com/a/49375740/11445093
 import os, sys
+from openbabel import OBMol, OBConversion
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 from initialize.parse_conf import parse_config, parse_config_genref
+from initialize.parse_docking_conf import parse_vina_conf, parse_plants_conf
 
 # Handle versioneer
 from ._version import get_versions
---
+++ hippos/tests/test_parse_docking_conf.py
@@ -0,0 +1,13 @@
+"""
+Test for functions in hippos.initialize.parse_docking_conf
+"""
+
+# Import package, test suite, and other packages as needed
+from hippos import parse_vina_conf, parse_plants_conf
+import pytest
+import os, sys
+
+def test_parse_vina_conf(tmpdir):
+    """Test parsing AutoDock Vina configuration"""
+
+    assert 0 == 0
