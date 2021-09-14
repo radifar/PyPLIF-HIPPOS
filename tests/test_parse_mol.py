936a6d4251c77c8b432bcbabafc9204184e819e0
radifar <m.radifar05@gmail.com>
2021-09-14 11:16:42 +0700

    add skeleton parse_mol module test

---
+++ tests/test_parse_mol.py
@@ -0,0 +1,32 @@
+"""
+Testing parse_mol modules, consists of 4 functions:
+
+parse_ligands(ligand_list: List[string]) -> List[OBMol]
+parse_protein(protein: string) -> OBMol
+
+        Mutate input list
+enumerate_ligand_files(
+    ligand_pose -> List[None],
+    ligand_files -> List[string]
+) -> None
+enumerate_ligand_file_list(
+    ligand_pose -> List[None],
+    ligand_file_list -> List[string]
+) -> None
+"""
+
+
+def test_parse_ligands():
+    pass
+
+
+def test_parse_protein():
+    pass
+
+
+def test_enumerate_ligand_files():
+    pass
+
+
+def test_enumerate_ligand_file_list():
+    pass
