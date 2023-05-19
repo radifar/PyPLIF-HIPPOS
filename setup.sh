b398d573bf92133c1b9b86be5f6ef468b0c68bec
radifar <m.radifar05@gmail.com>
2023-05-19 20:53:29 +0700

    feat: simplify installation process

--- setup.sh
+++ setup.sh
@@ -1,80 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-# If argument present do this
-
-if [ $1 > 0 ]  ; then
-    if  [ $1 = 'uninstall' ]; then
-        INSTALLDIR=$(cat ~/.bashrc | grep hippos.py | cut -d"'" -f 2 | sed "s/hippos.py//")
-        if [ $INSTALLDIR ] ; then
-            rm -r $INSTALLDIR
-            gawk '$0 !~/alias hippos/ { print $0 }' $HOME/.bashrc > .bashrc.tmp
-            cat .bashrc.tmp > $HOME/.bashrc
-            echo "HIPPOS has been successfully uninstalled"
-        else
-            echo "HIPPOS never been installed before"
-        fi
-    elif [ $1 = 'force' ]; then
-        INSTALLDIR=$(cat ~/.bashrc | grep hippos.py | cut -d"'" -f 2 | sed "s/hippos.py//")
-        if [ $INSTALLDIR ]; then
-            rm -r $INSTALLDIR
-        else
-            echo "alias hippos='$INSTALLDIR/hippos.py'" >> $HOME/.bashrc
-            echo "alias hippos-genref='$INSTALLDIR/hippos_genref.py'" >> $HOME/.bashrc
-        fi
-        mkdir -p $INSTALLDIR && rmdir $INSTALLDIR && cp -r pyplif_hippos $INSTALLDIR
-        chmod +x $INSTALLDIR/hippos.py
-        chmod +x $INSTALLDIR/hippos_genref.py
-        # alias hippos='$HOME/.hippos/hippos.py'
-        # alias hippos-genref='$HOME/.hippos/hippos_genref.py'
-        echo "HIPPOS successfully installed"
+if [ $1 > 0 ] ; then
+    if [ $1 = 'uninstall' ]; then
+        cp ~/.bashrc .bashrc-copy
+        grep -Ev hippos .bashrc-copy > ~/.bashrc
+        rm .bashrc-copy
+        echo "PyPLIF HIPPOS has been successfully uninstalled"
+        echo "PyPLIF HIPPOS will not be able to run the next time"
+        echo "you open new terminal"
     else
-        echo "Wrong Argument"
+        echo "$1 command is not recognized"
     fi
-# When no argument present do this
-else  
-    if  cat "$HOME/.bashrc" | grep "hippos" > /dev/null ; then
-        echo "HIPPOS installation failed because HIPPOS is already installed"
-        echo "Use option 'force' to overwrite the old installation:"
-        echo "./setup.sh force"
-        exit
-    else
-        read -p "HIPPOS will be installed in $HOME/.hippos, do you want to install it in another directory?\
-`echo $'\n> '`[y]es/[n]o, default: n `echo $'\n> '`" NODEFAULT
-        if [ "$NODEFAULT" = '' ] || [ "$NODEFAULT" = 'n' ]; then
-            if [ -e $HOME/.hippos ]; then
-                rm -r $HOME/.hippos
-            fi
-            cp -r src/pyplif_hippos $HOME/.hippos
-            chmod +x $HOME/.hippos/hippos.py
-            chmod +x $HOME/.hippos/hippos_genref.py
-            echo "alias hippos='$HOME/.hippos/hippos.py'" >> $HOME/.bashrc
-            echo "alias hippos-genref='$HOME/.hippos/hippos_genref.py'" >> $HOME/.bashrc
-            # alias hippos='$HOME/.hippos/hippos.py'
-            # alias hippos-genref='$HOME/.hippos/hippos_genref.py'
-            echo "HIPPOS successfully installed"
-        elif [ "$NODEFAULT" = 'y' ]; then
-            read -p "Enter the installation directory: " INSTALLDIR
-            if [ "$INSTALLDIR" = '' ]; then
-                echo "You are entering empty directory"
-                echo "Exiting now"
-                exit
-            elif [ -e $INSTALLDIR ]; then
-                echo "Installation directory already exist"
-                echo "Aborting installation"
-                exit
-            else 
-                mkdir -p $INSTALLDIR && rmdir $INSTALLDIR && cp -r src/pyplif_hippos $INSTALLDIR
-                chmod +x $INSTALLDIR/hippos.py
-                chmod +x $INSTALLDIR/hippos_genref.py
-                echo "alias hippos='$INSTALLDIR/hippos.py'" >> $HOME/.bashrc
-                echo "alias hippos-genref='$INSTALLDIR/hippos_genref.py'" >> $HOME/.bashrc
-                echo "HIPPOS successfully installed"
-                exit
-            fi
-        else
-            echo "Wrong input, please enter 'y' or 'n'"
-            echo "exiting now"
-            exit
-        fi
-    fi
+else
+    sourcedir=`pwd`
+    cp ~/.bashrc ~/.bashrc-ori
+    grep -Ev hippos ~/.bashrc-ori > ~/.bashrc
+    echo "alias hippos='$sourcedir/src/pyplif_hippos/hippos.py'" >> ~/.bashrc
+    echo "alias hippos-genref='$sourcedir/src/pyplif_hippos/hippos_genref.py'" >> ~/.bashrc 
+    chmod u+x $sourcedir/src/pyplif_hippos/hippos.py
+    chmod u+x $sourcedir/src/pyplif_hippos/hippos_genref.py
+    echo "PyPLIF HIPPOS is ready for service"
+    echo "To use PyPLIF HIPPOS, you have to open new terminal or"
+    echo "run 'source ~/.bashrc' in this terminal."
 fi
